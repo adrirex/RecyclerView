86c0f639b36365bcfcb77b7fe56ba1583663d662
Adrian Beltran <abeltran.insmollet4@gmail.com>
2019-12-02 23:14:32 +0100

    Ganador

--- app/src/main/java/abeltran/example/recyclerview/CartasAdapter.java
+++ app/src/main/java/abeltran/example/recyclerview/CartasAdapter.java
@@ -1,6 +1,8 @@
 package abeltran.example.recyclerview;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,21 +11,25 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.List;
 
 import abeltran.example.recyclerview.Cartas;
 import abeltran.example.recyclerview.R;
 
 public class CartasAdapter extends RecyclerView.Adapter<CartasAdapter.CartasViewHolder> {
+
     private List<Cartas> items;
     private Context context;
     int contador = 0;
     int cartaPrimera = 0;
     int cartaGirada = 0;
     boolean noMeToques = false;
+    int aciertos;
+    boolean starting = true;
+    int time = 30;
+    CountDownTimer ct;
+    TextView textTimer;
 
     public CartasAdapter(List<Cartas> items, Context context) {
         this.items = items;
@@ -31,7 +37,7 @@ public class CartasAdapter extends RecyclerView.Adapter<CartasAdapter.CartasView
     }
 
     public  class CartasViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        // Campos respectivos de un item
+        //        // Campos respectivos de un item
         public ImageView imagen;
 
         public CartasViewHolder(View v) {
@@ -40,8 +46,24 @@ public class CartasAdapter extends RecyclerView.Adapter<CartasAdapter.CartasView
             imagen.setOnClickListener(this);
         }
 
+        @SuppressLint("WrongConstant")
         @Override
         public void onClick(View v) {
+            //if(starting){
+            //    starting = false;
+            //  ct = new CountDownTimer(30000,1000) {
+            //      @Override
+            //      public void onTick(long millisUntilFinished) {
+            //          time--;
+            //          textTimer.setText(String.valueOf(time));
+            //      }
+
+            //      @Override
+            //      public void onFinish() {
+            //          Toast.makeText(context, "GAME OVER", Toast.LENGTH_SHORT).show();
+            //      }
+            //  }.start();
+            //}
             if(!noMeToques) {
             if(contador < 2 && items.get(getAdapterPosition()).getEstado() == Cartas.Estat.BACK) {
                 cartaGirada = getAdapterPosition();
@@ -55,7 +77,12 @@ public class CartasAdapter extends RecyclerView.Adapter<CartasAdapter.CartasView
             if(contador == 2){
                 if(items.get(cartaPrimera).getImagen() == items.get(cartaGirada).getImagen()){
                     contador = 0;
-                    //poner cartas en fixed
+                    aciertos++;
+                    if(aciertos == (items.size()) / 2){
+                        Toast.makeText(context, "ENHORABUENA HAS GANADO!",Toast.LENGTH_SHORT).show();
+                        noMeToques = true;
+                        //ct.cancel();
+                    }
                 }
                 else {
                     noMeToques = true;
@@ -74,8 +101,6 @@ public class CartasAdapter extends RecyclerView.Adapter<CartasAdapter.CartasView
             }
         }}
     }
-
-
 
     @Override
     public int getItemCount() {
--- app/src/main/java/abeltran/example/recyclerview/MainActivity.java
+++ app/src/main/java/abeltran/example/recyclerview/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collection;
